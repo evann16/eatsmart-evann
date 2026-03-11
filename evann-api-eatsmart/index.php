dab1bf0a79b02c8b3560b736a638deed767dca0e
evann16 <evannfa@gmail.com>
2026-03-11 20:45:20 +0000

    ajout header

--- evann-api-eatsmart/index.php
+++ evann-api-eatsmart/index.php
@@ -2,6 +2,7 @@
 require_once "controllers/ArticleController.php";
 require_once "controllers/CategorieController.php";
 require_once "controllers/CommandeController.php";
+header("Access-Control-Allow-Origin: *");
 
 $articleController = new ArticleController();
 $categorieController = new CategorieController();
